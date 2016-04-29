dde2b84a2d7067825d6ae32e202b22794e2fe029
Liam Childs <liam_childs@hotmail.com>
2016-04-29 11:08:41 +0200

    moved example to demo directory

--- demo/hilbert_in_treemap.R
+++ demo/hilbert_in_treemap.R
@@ -1,7 +1,5 @@
 library(dplyr)
-
-source('squarify.R')
-source('pseudo_hilbert.R')
+library(HilbertInTreemap)
 
 lengths = c(
   248956422, 242193529, 198295559, 190214555, 181538259, 170805979, 159345973,
@@ -9,7 +7,7 @@ lengths = c(
   101991189, 90338345, 83257441, 80373285, 58617616, 64444167, 46709983, 50818468
 )
 
-lengths = lengths / 100000
+lengths = lengths / 10000
 ratio = get.best.ratio(lengths)
 height = sqrt(sum(lengths) / ratio)
 width = height * ratio
@@ -31,17 +29,16 @@ rectangles = squarify.coordinates(rows, width, height) %>%
          width = x2 - x1,
          height = y1 - y2)
 
-par(mar=c(0, 0, 0, 0))
+par(mar = c(0,0,0,0), xaxs = "i", yaxs = "i")
 plot(1, type="n", axes=F, xlab="", ylab="", xlim=c(0, max(rectangles$x + rectangles$width)), ylim=c(0, max(rectangles$y + rectangles$height)))
-#rect(rectangles$x1, rectangles$y1, rectangles$x2, rectangles$y2)
+rect(rectangles$x1, rectangles$y1, rectangles$x2, rectangles$y2, border='red')
 
 for (i in 1:nrow(rectangles)) {
   rectangle = rectangles[i,]
-  points = pseudo.hilbert(1:lengths[i],
-                          rectangle$width + 2,
-                          rectangle$height + 2)
+  points = pseudo.hilbert(rectangle$width,
+                          rectangle$height)
    points = points %>%
-     mutate(x = x * 0.95 + rectangle$x + 0.05,
-            y = y * 0.95 + rectangle$y + 0.05)
+     mutate(x = x + rectangle$x + 0.5,
+            y = y + rectangle$y + 0.5)
   lines(points)
 }
